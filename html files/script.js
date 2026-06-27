64630c4445a91aed2e3e37abfdf130db771106ea
JanRa2 <jaanucpj0501@gmail.com>
2026-06-27 23:27:33 +0530

    Added registration Form Validation

--- html files/script.js
+++ html files/script.js
@@ -1,3 +1,40 @@
-function showSuccess() {
-    alert("Registration Successful!");
+function validateForm() {
+
+    let name = document.getElementById("name").value;
+    let date = document.getElementById("date").value;
+    let alergies = document.getElementById("alergies").value;
+    let contact = document.getElementById("contact").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let password = document.getElementById("password").value;
+
+    if (
+        name === "" ||
+        email === "" ||
+        phone === "" ||
+        date === "" ||
+        password === "" ||
+        contact === "" ||
+        alergies === ""
+    ) {
+        alert("Please fill all fields");
+    }
+    else if (phone.length !== 10) {
+        alert("Phone number must be 10 digits");
+    }
+    else if (!email.includes("@")) {
+        alert("Invalid Email");
+    }
+    else {
+
+        let patient = {
+            name: name,
+            email: email,
+            phone: phone
+        };
+
+        console.log(patient);
+
+        alert("Registration Successful!");
+    }
 }
